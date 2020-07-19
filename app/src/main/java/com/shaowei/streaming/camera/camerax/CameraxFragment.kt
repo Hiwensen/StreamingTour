0084be2e4a2a10911b31b7f4a29c2dc7decf723f
shaowei.chang <hpu1861@gmail.com>
2020-07-19 13:31:30 +0800

    Add CameraX implementation

--- app/src/main/java/com/shaowei/streaming/camera/camerax/CameraxFragment.kt
+++ app/src/main/java/com/shaowei/streaming/camera/camerax/CameraxFragment.kt
@@ -3,8 +3,11 @@ package com.shaowei.streaming.camera.camerax
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.display.DisplayManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -14,11 +17,22 @@ import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import java.io.File
-import java.util.concurrent.ExecutorService
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.shaowei.streaming.MainActivity
 import com.shaowei.streaming.R
+import com.shaowei.streaming.hasCameraPermission
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 typealias LumaListener = (luma: Double) -> Unit
 
@@ -60,8 +74,137 @@ class CameraxFragment : Fragment() {
         }
     }
 
+    /**
+     * We need a display listener for orientation changes that do not trigger a configuration
+     * change, for example if we choose to override config change in manifest or for 180-degree
+     * orientation changes.
+     */
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayChanged(displayId: Int) =
+            view?.let {
+                if (displayId == this@CameraxFragment.displayId) {
+
+                    Log.d(TAG, "rotation changed: ${it.display?.rotation}")
+                    imageCapture?.targetRotation = it.display.rotation
+                    imageAnalyzer?.targetRotation = it.display.rotation
+                }
+            } ?: Unit
+
+        override fun onDisplayAdded(displayId: Int) = Unit
+        override fun onDisplayRemoved(displayId: Int) = Unit
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
             View? = inflater.inflate(R.layout.fragment_camerax, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        container = view as ConstraintLayout
+        viewFinder = container.findViewById(R.id.viewFinder)
 
+        // Initialize background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        broadcastManager = LocalBroadcastManager.getInstance(view.context)
+
+        // Set up the intent filter that will receive events from main activity
+        val filter = IntentFilter().apply { addAction(KEY_EVENT_ACTION) }
+        broadcastManager.registerReceiver(volumeDownReceiver,filter)
+
+        // Every time the orientation of the device changes, update the rotation for use case
+        displayManager.registerDisplayListener(displayListener, null)
+
+        // Determine the output directory
+        outputDirectory = getOutputDirectory(requireContext())
+
+        // Wait for the views to be properly laid out
+        viewFinder.post {
+            // Keep track of the display in which this view is attached
+            displayId = viewFinder.display.displayId
+
+            // Build UI controls
+            updateCameraUi()
+
+            // Set up the camera and its use cases
+            setUpCamera()
+        }
+    }
+
+    private fun setUpCamera() {
+
+    }
+
+    /**
+     * Method used to re-draw the camera UI controls, called every time configuration changes.
+     */
+    private fun updateCameraUi() {
+        // Remove previous UI if any
+        container.findViewById<ConstraintLayout>(R.id.camera_ui_container)?.let {
+            container.removeView(it)
+        }
+
+        // Inflate a new camera controller view
+        val controllerView = View.inflate(requireContext(), R.layout.camera_ui_container, container)
+
+        // In the background, load latest photo taken(if any) for gallery thumbnail
+        lifecycleScope.launch(Dispatchers.IO) {
+            outputDirectory.listFiles{ file ->
+                EXTENSION_WHITELIST.contains(file.extension.toUpperCase(Locale.ROOT))
+            }?.max()?.let {
+                setGalleryThumbnail(Uri.fromFile(it))
+            }
+        }
+
+        // todo
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!hasCameraPermission(requireContext())) {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container)
+                .navigate(CameraxFragmentDirections.actionCameraToPermissions())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Shut down background executor
+        cameraExecutor.shutdown()
+
+        // Unregister broadcast receivers and listeners
+        broadcastManager.unregisterReceiver(volumeDownReceiver)
+        displayManager.unregisterDisplayListener(displayListener)
+    }
+
+
+    private fun setGalleryThumbnail(uri: Uri) {
+        // Reference of the view that holds the gallery thumbnail
+        val thumbnail = container.findViewById<ImageButton>(R.id.photo_view_button)
+
+        // Run the operations in the view's thread
+        thumbnail.post {
+
+            // Remove thumbnail padding
+            thumbnail.setPadding(resources.getDimension(R.dimen.stroke_small).toInt())
+
+            // Load thumbnail into circular button using Glide
+            Glide.with(thumbnail)
+                .load(uri)
+                .apply(RequestOptions.circleCropTransform())
+                .into(thumbnail)
+        }
+    }
+
+    companion object {
+        val TAG = CameraxFragment::class.java.simpleName
+
+        fun getOutputDirectory(context: Context): File {
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() } }
+            return if (mediaDir != null && mediaDir.exists())
+                mediaDir else appContext.filesDir
+        }
+    }
 }
