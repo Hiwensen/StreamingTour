b8400ef794cedfb0ea81f3773634f70521cd4c5e
Shaowei.Chang <hpu1861@gmail.com>
2020-10-07 16:47:24 +0800

    Add media extractor

--- app/src/main/java/com/shaowei/streaming/mediaExtractor/MediaExtractorActivity.kt
+++ app/src/main/java/com/shaowei/streaming/mediaExtractor/MediaExtractorActivity.kt
@@ -2,22 +2,31 @@ package com.shaowei.streaming.mediaExtractor
 
 import android.media.MediaExtractor
 import android.media.MediaFormat
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.shaowei.streaming.R
+import java.io.File
+import java.io.FileOutputStream
 import java.nio.ByteBuffer
 
 class MediaExtractorActivity : AppCompatActivity() {
     private lateinit var extractFile: Button
     private val mExtractor = MediaExtractor()
     private val BUFFER_CAPACITY = 1000
-    private val mFilePath = ""
+    private var mFilePath = Uri.parse("android.resource://com.shaowei.streaming/raw/shariver").toString()
+    private val mVideoPath = "/sdcard/extractor/video.mp4"
+    private val mAudioPath = "/sdcard/extractor/audio.pcm"
+    private val mVideoOutputStream = FileOutputStream(File(mVideoPath))
+    private val mAudioOutputStream = FileOutputStream(File(mAudioPath))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_extractor)
         extractFile = findViewById(R.id.extract_mp4)
+        Log.e("chang",mFilePath)
         extractFile.setOnClickListener { extract(mFilePath) }
     }
 
