24252f9c4f73f20191d12cd756339d2530c83342
shaowei.chang <hpu1861@gmail.com>
2021-04-10 12:49:37 +0800

    Wrap up H264Player

--- app/src/main/java/com/shaowei/streaming/mediacodec/H264Player.kt
+++ app/src/main/java/com/shaowei/streaming/mediacodec/H264Player.kt
@@ -1,111 +1,174 @@
 package com.shaowei.streaming.mediacodec
 
+import android.content.Context
 import android.media.MediaCodec
+import android.media.MediaCodecList
 import android.media.MediaFormat
 import android.util.Log
 import android.view.Surface
+import android.widget.Toast
 import java.io.ByteArrayOutputStream
-import java.io.DataInputStream
-import java.io.File
-import java.io.FileInputStream
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.io.IOException
+import java.io.InputStream
 
 class H264Player {
     private val TAG = H264Player::class.java.simpleName
-
-    private val mExcutor: ExecutorService = Executors.newFixedThreadPool(3)
-    private lateinit var mMediaCodec: MediaCodec
-    private lateinit var mFilePath: String
-    private var mWidth = 0
-    private var mHeight = 0
-    private val TIMEOUT_MICRO_SECONDS = 10000L
+    private val mMediaCodec: MediaCodec
+    private val VIDEO_TYPE_H264 = "video/avc"
+    private val FIND_NEXT_FRAME_OFFSET = 1
+    private var mOutputFormat: MediaFormat? = null
+    private val mContentWidth = 368
+    private val mContentHeight = 384
+    private val CODEC_DEQUEUE_TIMEOUT_US = 10000L
 
     init {
-        try {
-            mMediaCodec = MediaCodec.createDecoderByType("video/avc")
-        } catch (exception: Exception) {
-            Log.e(TAG, "create media codec failed:$exception")
-        }
+        mMediaCodec = MediaCodec.createDecoderByType(VIDEO_TYPE_H264)
     }
 
-    fun play(filePath: String, surface: Surface) {
-        mFilePath = filePath
-        val mediaFormat = MediaFormat.createVideoFormat("video/avc", mWidth, mHeight)
-        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 15)
-        mMediaCodec.configure(mediaFormat, surface, null, 0)
+    fun playAsync(rawFileId: Int, targetSurface: Surface, context: Context) {
+        val bytes = getBytes(context, rawFileId)
+        val sourceFileSize = bytes.size
+        Log.d(TAG, "sourceFileSize:$sourceFileSize")
+        var startIndex = 0
+
+        mMediaCodec.setCallback(object : MediaCodec.Callback() {
+            override fun onOutputBufferAvailable(codec: MediaCodec, index: Int, info: MediaCodec.BufferInfo) {
+                if (index >= 0) {
+                    codec.releaseOutputBuffer(index, true)
+                }
+            }
+
+            override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
+                Log.d(TAG, "onInputBufferAvailable,index:$index")
+                codec.getInputBuffer(index)?.let {
+                    it.clear()
+                    val nextFrameStartPosition =
+                        findNextFrameStartPosition(bytes, startIndex + FIND_NEXT_FRAME_OFFSET, sourceFileSize)
+                    if (sourceFileSize == 0 || nextFrameStartPosition < 0 || startIndex >= sourceFileSize) {
+                        return
+                    }
+
+                    it.put(bytes, startIndex, nextFrameStartPosition - startIndex)
+                    codec.queueInputBuffer(index, 0, nextFrameStartPosition - startIndex, 0, 0)
+                    startIndex = nextFrameStartPosition
+                }
+            }
+
+            override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
+                mOutputFormat = format
+            }
+
+            override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
+                Log.e(TAG, "MediaCodec error:$e")
+            }
+
+        })
+
+        val videoFormat = MediaFormat.createVideoFormat(VIDEO_TYPE_H264, mContentWidth, mContentHeight)
+        videoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 15)
+        mMediaCodec.configure(videoFormat, targetSurface, null, 0)
+        mOutputFormat = mMediaCodec.outputFormat
         mMediaCodec.start()
-        mExcutor.execute(DecodeH264(filePath))
     }
 
-    inner class DecodeH264(private val filePath: String) : Runnable {
-        override fun run() {
-            decodeH264(filePath)
+    fun playSync(rawFileId: Int, targetSurface: Surface, context: Context) {
+        val bytes = getBytes(context, rawFileId)
+        val sourceFileSize = bytes.size
+        if (sourceFileSize == 0) {
+            Toast.makeText(context, "fail to get srouce data", Toast.LENGTH_SHORT).show()
+            return
         }
 
-        private fun decodeH264(filePath: String) {
-            val fileBytes = getFileBytes(filePath)
-            val totalSize = fileBytes.size
-            var startIndex = 0
+        Log.d(TAG, "sourceFileSize:$sourceFileSize")
+        var startIndex = 0
 
+        val mediaFormat = MediaFormat.createVideoFormat(VIDEO_TYPE_H264, mContentWidth, mContentHeight)
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 15)
+        mMediaCodec.configure(mediaFormat, targetSurface, null, 0)
+        mOutputFormat = mMediaCodec.outputFormat
+        mMediaCodec.start()
+
+        Thread {
             while (true) {
-                if (totalSize == 0 || startIndex >= totalSize) {
+                val nextFrameStartPosition =
+                    findNextFrameStartPosition(bytes, startIndex + FIND_NEXT_FRAME_OFFSET, sourceFileSize)
+                if (nextFrameStartPosition < 0) {
+                    Log.e(TAG, "fail to get next frame start position")
                     break
                 }
 
-                val nextFrameStart = findFrameStart(fileBytes, totalSize, startIndex + 2)
-                val availableIndex = mMediaCodec.dequeueInputBuffer(TIMEOUT_MICRO_SECONDS)
-                if (availableIndex > 0) {
-                    // Found available input buffer, load it to mediaCodec
-                    val inputBuffer = mMediaCodec.getInputBuffer(availableIndex)
-                    inputBuffer?.run {
-                        inputBuffer.clear()
-                        inputBuffer.put(fileBytes, startIndex, (nextFrameStart - startIndex))
-                        mMediaCodec.queueInputBuffer(availableIndex, startIndex, nextFrameStart - startIndex, 0, 0)
-                        startIndex = nextFrameStart
+                if (nextFrameStartPosition >= sourceFileSize) {
+                    Log.d(TAG, "decode file end")
+                    break
+                }
+
+                // Load data into mediaCoded
+                val inputBufferIndex = mMediaCodec.dequeueInputBuffer(CODEC_DEQUEUE_TIMEOUT_US)
+                if (inputBufferIndex >= 0) {
+                    mMediaCodec.getInputBuffer(inputBufferIndex)?.let {
+                        it.clear()
+                        val dataSize = nextFrameStartPosition - startIndex
+                        it.put(bytes, startIndex, dataSize)
+                        mMediaCodec.queueInputBuffer(inputBufferIndex, 0, dataSize, 0, 0)
+                        startIndex = nextFrameStartPosition
                     }
                 } else {
                     continue
                 }
-            }
 
-            // Get output data from mediaCodec
-            val bufferInfo = MediaCodec.BufferInfo()
-            val outputBufferIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, TIMEOUT_MICRO_SECONDS)
-            if (outputBufferIndex >= 0) {
-                mMediaCodec.releaseOutputBuffer(outputBufferIndex, true)
-            }
-        }
-
-        private fun findFrameStart(fileBytes: ByteArray, totalSize: Int, startIndex: Int): Int {
-            for (i in startIndex until totalSize - 4) {
-                if (fileBytes[i].toInt() == 0x00 && fileBytes[i + 1].toInt() == 0x00 && fileBytes[i + 2].toInt() ==
-                    0x00 && fileBytes[i + 3].toInt() == 0x01) {
-                    return i
+                // Get output data
+                val bufferInfo = MediaCodec.BufferInfo()
+                val outputIndex = mMediaCodec.dequeueOutputBuffer(bufferInfo, CODEC_DEQUEUE_TIMEOUT_US)
+                if (outputIndex >= 0) {
+                    mMediaCodec.releaseOutputBuffer(outputIndex, true)
                 }
             }
-            return -1
-        }
-
-        private fun getFileBytes(filePath: String): ByteArray {
-            val inputStream = DataInputStream(FileInputStream(File(filePath)))
-            val byteArrayOutputStream = ByteArrayOutputStream()
-
-            val size = 1024
-            var length: Int
-            val byteArray = ByteArray(size)
-            while (true) {
-                length = inputStream.read(byteArray, 0, size)
-                if (length == -1) {
-                    break
-                }
-                byteArrayOutputStream.write(byteArray, 0, length)
-            }
-
-            return byteArrayOutputStream.toByteArray()
-        }
-
+        }.start()
     }
 
+    fun stop() {
+        mMediaCodec.stop()
+        mMediaCodec.release()
+    }
 
+    @Throws(IOException::class)
+    private fun getBytes(context: Context, rawFileId: Int): ByteArray {
+        val `is`: InputStream = context.resources.openRawResource(rawFileId)
+        var len: Int
+        val size = 1024
+        val byteArray = ByteArray(size)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        while (`is`.read(byteArray, 0, size).also { len = it } != -1) byteArrayOutputStream.write(byteArray, 0, len)
+        return byteArrayOutputStream.toByteArray()
+    }
+
+    private fun findNextFrameStartPosition(bytes: ByteArray, start: Int, totalSize: Int): Int {
+        // In the h264 stream, the separator between each frame is 0x00 00 00 01
+        for (i in start until totalSize - 4) {
+            if (bytes[i].toInt() == 0x00 && bytes[i + 1].toInt() == 0x00 && bytes[i + 2].toInt() == 0x00 && bytes[i +
+                        3].toInt() == 0x01) {
+                Log.d(TAG, "findNextFrameStartPosition:$i")
+                return i
+            }
+        }
+        return -1
+    }
+
+    private fun findCodecNameForFormat(encoder: Boolean, format: MediaFormat): String {
+        val mime = format.getString(MediaFormat.KEY_MIME)
+        val mediaCodecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
+        val codecInfos = mediaCodecList.codecInfos
+        for (codecInfo in codecInfos) {
+            if (codecInfo.isEncoder != encoder) {
+                continue
+            }
+
+            val capabilitiesForType = codecInfo.getCapabilitiesForType(mime)
+            if (capabilitiesForType != null && capabilitiesForType.isFormatSupported(format)) {
+                return codecInfo.name
+            }
+        }
+
+        return ""
+    }
 }
--- app/src/main/java/com/shaowei/streaming/mediacodec/MediaCodecIndexActivity.kt
+++ app/src/main/java/com/shaowei/streaming/mediacodec/MediaCodecIndexActivity.kt
@@ -1,6 +1,5 @@
 package com.shaowei.streaming.mediacodec
 
-import android.media.MediaCodec
 import android.media.MediaCodecList
 import android.media.MediaCodecList.REGULAR_CODECS
 import android.media.MediaFormat
@@ -10,132 +9,59 @@ import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shaowei.streaming.R
 
 private val TAG = MediaCodecIndexActivity::class.java.simpleName
 
 class MediaCodecIndexActivity : AppCompatActivity() {
-    private var mIsRecording: Boolean = false
-    private lateinit var mAudioFormat: MediaFormat
-    private lateinit var mAudioCodec: MediaCodec
     private lateinit var mVideoFormat: MediaFormat
-    private lateinit var mVideoCodec: MediaCodec
-    private val mH264Player = H264AsyncPlayer()
-
-    private var mOutputFormat: MediaFormat? = null
+    private val mH264Player = H264Player()
+    private lateinit var mSurface:Surface
+    private var mSurfaceReady = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_codec)
-        findViewById<Button>(R.id.audio_encode).setOnClickListener {
-            encodeAudio()
+
+        findViewById<Button>(R.id.video_decode_async).setOnClickListener{
+            if (mSurfaceReady) {
+                mH264Player.playAsync(R.raw.sample, mSurface, this)
+            } else {
+                Toast.makeText(this,"surface is not ready",Toast.LENGTH_SHORT).show()
+            }
         }
 
-        findViewById<Button>(R.id.audio_decode).setOnClickListener {
-            decodeAudio()
-        }
-
-        findViewById<Button>(R.id.video_encode).setOnClickListener {
-            encodeVideo()
+        findViewById<Button>(R.id.video_decode_sync).setOnClickListener{
+            if (mSurfaceReady) {
+                mH264Player.playSync(R.raw.sample, mSurface, this)
+            } else {
+                Toast.makeText(this,"surface is not ready",Toast.LENGTH_SHORT).show()
+            }
         }
 
         val surfaceView = findViewById<SurfaceView>(R.id.surface_view)
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-
+                Log.d(TAG, "surfaceChanged")
             }
 
             override fun surfaceDestroyed(holder: SurfaceHolder?) {
-
+                Log.d(TAG, "surfaceDestroyed")
             }
 
             override fun surfaceCreated(holder: SurfaceHolder) {
-                decodeVideo(holder.surface)
+                mSurfaceReady = true
+                mSurface = holder.surface
             }
 
         })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        initCodec()
     }
 
     override fun onStop() {
         super.onStop()
         mH264Player.stop()
-    }
-
-    private fun initCodec() {
-
-    }
-
-    private fun decodeAudio() {
-
-    }
-
-    private fun encodeAudio() {
-        mAudioCodec.setCallback(object : MediaCodec.Callback() {
-            override fun onOutputBufferAvailable(codec: MediaCodec, index: Int, info: MediaCodec.BufferInfo) {
-                val outputBuffer = codec.getOutputBuffer(index)
-                val bufferFormat = codec.getOutputFormat(index)
-                // bufferFormat is equivalent to mOutputFormat
-                // outputBuffer is ready to be processed or rendered.
-                // ...
-
-                //  codec.releaseOutputBuffer(index,)
-            }
-
-            override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
-                val inputBuffer = codec.getInputBuffer(index)
-                // fill inputBuffer with valid data
-                //...
-                //codec.queueInputBuffer(index,)
-            }
-
-            override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
-                // Subsequent data will conform to new format.
-                // Can ignore if using getOutputFormat(outputBufferId)
-                mOutputFormat = format; // option B
-            }
-
-            override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
-                Log.e(TAG, "mediaCodec error:$e")
-            }
-
-        })
-
-        mAudioCodec.configure(mVideoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
-        mOutputFormat = mAudioCodec.outputFormat
-        mAudioCodec.start()
-    }
-
-    private fun decodeVideo(surface: Surface) {
-        mH264Player.playAsync("", surface, this)
-//        mH264Player.playSync("", surface, this)
-    }
-
-    private fun encodeVideo() {
-
-    }
-
-    private fun findCodecNameForFormat(encoder: Boolean, format: MediaFormat): String {
-        val mime = format.getString(MediaFormat.KEY_MIME)
-        val mediaCodecList = MediaCodecList(REGULAR_CODECS)
-        val codecInfos = mediaCodecList.codecInfos
-        for (codecInfo in codecInfos) {
-            if (codecInfo.isEncoder != encoder) {
-                continue
-            }
-
-            val capabilitiesForType = codecInfo.getCapabilitiesForType(mime)
-            if (capabilitiesForType != null && capabilitiesForType.isFormatSupported(format)) {
-                return codecInfo.name
-            }
-        }
-
-        return ""
     }
 
 }
