32b56c56f5c28221741e4f713d811ae40161873b
shaowei.chang <hpu1861@gmail.com>
2021-10-09 22:20:40 +0800

    Disable audio mix

--- app/src/main/java/com/shaowei/streaming/audio/mix/AudioMixActivity.kt
+++ app/src/main/java/com/shaowei/streaming/audio/mix/AudioMixActivity.kt
@@ -18,8 +18,8 @@ class AudioMixActivity : AppCompatActivity() {
 //        val videoFile = File("android.resource://$packageName/", "audio_mix_original_video.mp4")
         val musicFile = File("android.resource://$packageName/", "audio_mix_music.mp3")
 
-        AudioMixer().mixAudioTrack(originalVideoFilePath = "",originalMusicFilePath = musicFile.absolutePath,
-        mixedAudioPath = "",cacheDir = cacheDir,startTimeUs = 10*1000*1000,endTimeUs = 20*1000*1000,
-            originalVideoVolume = 2, musicVolume = 8)
+//        AudioMixer().mixAudioTrack(originalVideoFilePath = "",originalMusicFilePath = musicFile.absolutePath,
+//        mixedAudioPath = "",cacheDir = cacheDir,startTimeUs = 10*1000*1000,endTimeUs = 20*1000*1000,
+//            originalVideoVolume = 2, musicVolume = 8)
     }
 }
