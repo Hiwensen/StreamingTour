5c1a0938238e365782b9eea423733fe747d987bf
shaowei.chang <hpu1861@gmail.com>
2021-10-25 09:06:32 +0800

    Add VideoProcessor

--- app/src/main/java/com/shaowei/streaming/video/VideoProcessor.kt
+++ app/src/main/java/com/shaowei/streaming/video/VideoProcessor.kt
@@ -1,15 +1,21 @@
 package com.shaowei.streaming.video
 
+import android.media.MediaExtractor
+import android.media.MediaFormat
 import android.util.Log
 import java.io.File
 
 class VideoProcessor {
     private val TAG = VideoProcessor::class.java.simpleName
+    private val TRACK_UNKNOWN = -1
 
-    fun clipAndMixVideo(originalVideoPath: String, videoStartPosition: Float, videoEndPosition: Float,
-        backgroundMusicPath: String,outputVideoPath: String, cacheDir: File ) {
+    fun clipAndMixVideo(
+        originalVideoPath: String, startPosition: Int, endPosition: Int,
+        backgroundMusicPath: String, outputVideoPath: String, cacheDir: File
+    ) {
         // Extract and decode original audio to PCM
         val originalAudioPCMFile = File(cacheDir, "originalAudio.pcm")
+        decodeAudioFileToPCM(originalVideoPath, startPosition, endPosition, originalAudioPCMFile.absolutePath)
 
 
         // Decode background music to PCM
@@ -22,13 +28,40 @@ class VideoProcessor {
 
     }
 
-    private fun decodeAudioFileToPCM(originalFilePath:String, startTimeUS:Int, endTimeUs:Int,
-                                        outputPCMFilePath:String) {
+    private fun decodeAudioFileToPCM(
+        originalFilePath: String, startTimeUS: Int, endTimeUs: Int,
+        outputPCMFilePath: String
+    ) {
         if (endTimeUs > startTimeUS) {
             Log.e(TAG, "decodeAudioFileToPCM, start time greater than end time")
         }
+        val mediaExtractor = MediaExtractor()
+        mediaExtractor.setDataSource(originalFilePath)
+        val audioTrack = getTrack(mediaExtractor, true)
+        if (audioTrack == TRACK_UNKNOWN) {
+            Log.e(TAG,"fail to get track")
+            return
+        }
 
+        mediaExtractor.selectTrack(audioTrack)
 
+    }
+
+    private fun getTrack(mediaExtractor: MediaExtractor, audio: Boolean): Int {
+        val trackCount = mediaExtractor.trackCount
+        for (i in 0 until trackCount) {
+            val trackFormat = mediaExtractor.getTrackFormat(i)
+            val mime = trackFormat.getString(MediaFormat.KEY_MIME)
+            if (mime?.startsWith("/video") == true && !audio) {
+                return i
+            }
+
+            if (mime?.startsWith("/audio") == true && audio) {
+                return i
+            }
+        }
+
+        return TRACK_UNKNOWN
     }
 
 }
