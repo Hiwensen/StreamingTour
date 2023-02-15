ddd26f56a6cdd235b8fb1583e06e08824cdbfb56
shaowei.chang <hpu1861@gmail.com>
2023-02-15 09:03:41 +0800

    Add ExoPlayer

---
+++ app/src/main/java/com/shaowei/streaming/exoplayer/ExoPlayerActivity.kt
@@ -0,0 +1,22 @@
+package com.shaowei.streaming.exoplayer
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExoPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
